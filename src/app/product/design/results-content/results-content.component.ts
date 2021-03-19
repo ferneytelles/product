d0209bebfbb14d1e2730b414a43cb4026b5dbb68
Unknown <ferney.telles@unillanos.edu.co>
2021-03-19 17:03:16 -0500

    02

--- src/app/product/design/results-content/results-content.component.ts
+++ src/app/product/design/results-content/results-content.component.ts
@@ -9,7 +9,7 @@ export class ResultsContentComponent implements OnInit {
 
   imgs = ['assets/img/copa.png', 'assets/img/basketball.png', 'assets/img/basketball2.png', 'assets/img/bandera.png', 'assets/img/garra.png', 'assets/img/deportista.png', 'assets/img/espirales.png'];
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void { }
 
