4ff67f662626eef1696d95fa130537a5660a2427
ferney <ferney.telles@unillanos.edu.co>
2021-05-05 18:09:57 -0500

    04

--- src/app/product/categories/categories.component.ts
+++ src/app/product/categories/categories.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+/**
+ * Componente que contiene la landing de categoría
+ */
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -8,11 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CategoriesComponent implements OnInit {
 
+  /**
+   * Arreglo que contiene las opciones del menu
+   */
   menu = ['Camisetas', 'Sacos', 'Gorras', 'Hoodies', 'Polos', 'Tazas', 'Libretas', 'Bolsas Ecológicas', 'Tennis', 'Posters', 'Mochila', 'Cubiertas de celular', 'Chaquetas'];
+  /**
+   * Arreglo que contiene la ruta de las imagenes
+   */
   images = {banner: '', image1: '', image2: '', image3: '', image4: '', image5: '', image6: '', image7: '', image8: '', image9: ''};
+  /**
+   * Cadena la categoría recibida por parametro
+   */
   categori: string;
+  /**
+   * Cadena que contiene el texto para el banner superior
+   */
   text: string;
 
+  /**
+   * Contructor que llama al modulo de la ruta activa, y se asigna
+   * la ruta de las imagenes correspondientes dependiendo la categoría
+   * @param route Modulo que permite recibir los parametros de la ruta
+   */
   constructor(private route: ActivatedRoute) {
     route.params.subscribe( data => {
       if (data.categori === 'hombres'){
