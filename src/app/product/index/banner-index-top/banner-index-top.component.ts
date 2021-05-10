1d6707e3ccb40e7abf7238393dae8454ab7b7b69
ferney <ferney.telles@unillanos.edu.co>
2021-05-10 13:10:28 -0500

    10/05/2021

--- src/app/product/index/banner-index-top/banner-index-top.component.ts
+++ src/app/product/index/banner-index-top/banner-index-top.component.ts
@@ -8,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerIndexTopComponent implements OnInit {
 
-  constructor() { }
+  banner1 = '';
+  banner2 = '';
+
+  constructor() {
+    if (window.innerWidth > 960){
+      this.banner1 = 'assets/img/index/indexBannerLeft.png';
+      this.banner2 = 'assets/img/index/indexBannerRight.png';
+    }else{
+      this.banner1 = 'assets/img/index/bannerMovil1.png';
+      this.banner2 = 'assets/img/index/bannerMovil2.png';
+    }
+   }
 
   ngOnInit(): void {
   }
