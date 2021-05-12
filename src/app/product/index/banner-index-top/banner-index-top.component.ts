f8ff476e026543e56731608a21e6d5d759f30f17
ferney <ferney.telles@unillanos.edu.co>
2021-05-12 10:22:15 -0500

    02
    documentación de index en proceso

--- src/app/product/index/banner-index-top/banner-index-top.component.ts
+++ src/app/product/index/banner-index-top/banner-index-top.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-
+/**
+ * Componente que contiene el banner superior de la index.
+ */
 @Component({
   selector: 'app-banner-index-top',
   templateUrl: './banner-index-top.component.html',
@@ -8,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerIndexTopComponent implements OnInit {
 
+  /**
+   * Cadena que contiene la ruta de la primera imagen del banner.
+   */
   banner1 = '';
+  /**
+   * Cadena que contiene la ruta de la segunda imagen del banner.
+   */
   banner2 = '';
 
+  /**
+   * Contructor que asigna la ruta de la imagen dependiendo
+   * del tamaño del dispositivo
+   */
   constructor() {
     if (window.innerWidth > 960){
       this.banner1 = 'assets/img/index/indexBannerLeft.png';
